abb24b703e552327e994f10beb40c04c99858fce
muhammadxoja <muhammadxojaofficial@gmail.com>
2026-03-05 00:50:44 +0500

    add urls

--- app/urls.py
+++ app/urls.py
@@ -16,7 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
+    path('', include('app.urls'))
     path('admin/', admin.site.urls),
+    path('register/', views.register_view, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    
+    path('', views.note_list, name='note_list'),
+    path('new/', views.note_create, name='note_create'),
+    path('delete/<int:pk>/', views.note_delete, name='note_delete'),
 ]
---
+++ app/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from .models import Note
+from django.shortcuts import get_object_or_404
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('note_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+
+@login_required
+def note_list(request):
+    notes = Note.objects.filter(user=request.user) # Only get notes for the logged-in user
+    return render(request, 'note_list.html', {'notes': notes})
+
+@login_required
+def note_create(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        Note.objects.create(user=request.user, title=title, content=content)
+        return redirect('note_list')
+    return render(request, 'note_form.html')
+
+@login_required
+def note_delete(request, pk):
+    note = get_object_or_404(Note, pk=pk, user=request.user)
+    if request.method == 'POST':
+        note.delete()
+        return redirect('note_list')
+    return render(request, 'note_confirm_delete.html', {'note': note})
