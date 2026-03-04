3dec0111062ec18bdb5b9a28b3f5ef330ab34334
muhammadxoja <muhammadxojaofficial@gmail.com>
2026-03-05 01:02:29 +0500

    removed some comments

--- app/urls.py
+++ app/urls.py
@@ -1,32 +1,17 @@
-"""
-URL configuration for app project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
-from django.urls import path
-from django.contrib.auth import views as auth_views
+from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', include('app.urls'))
     path('admin/', admin.site.urls),
+    
+    # User Auth
     path('register/', views.register_view, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     
+    # Note CRUD
     path('', views.note_list, name='note_list'),
     path('new/', views.note_create, name='note_create'),
     path('delete/<int:pk>/', views.note_delete, name='note_delete'),
